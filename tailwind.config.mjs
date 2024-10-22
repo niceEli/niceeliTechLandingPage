d0a6fda3c80a32c2bc4ec931fef587a5faa0074f
Eli <78963198+niceEli@users.noreply.github.com>
2024-10-22 09:46:45 -0400

    1

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "node_modules/daisyui/dist/**/*.js",
+    "node_modules/react-daisyui/dist/**/*.js",
+  ],
   theme: {
     extend: {},
   },
